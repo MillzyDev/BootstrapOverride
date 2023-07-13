bb02d871b564f8257e388dcc8867ad9675e33786
Millzy <millzyyt08@gmail.com>
2023-07-13 15:40:45 +0100

    patch patches

---
+++ BootstrapOverride/Mod.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+
+namespace BootstrapOverride
+{
+    public sealed class Mod : MelonMod
+    {
+        private HarmonyLib.Harmony _harmony = null!;
+
+        public override void OnInitializeMelon()
+        {
+            _harmony = new HarmonyLib.Harmony($"dev.millzy.{nameof(BootstrapOverride)}");
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            _harmony.PatchAll();
+        }
+
+        public override void OnDeinitializeMelon()
+        {
+            _harmony.UnpatchSelf();
+        }
+    }
+}
